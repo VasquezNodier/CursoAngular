285cd2b1e284118503b81b8a2d44fd89a6a616c5
Nodier Vasquez <vasqueznap@gmail.com>
2023-12-20 20:28:41 -0500

    01 basic types

--- TypeScript-Intro/src/main.ts
+++ TypeScript-Intro/src/main.ts
@@ -3,10 +3,12 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
+// import './topics/01-basic_types.ts'
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Vite
 `
 
-console.log("Hola mundo");
+// console.log("Hola mundo");
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
---
+++ TypeScript-Intro/src/topics/01-basic_types.ts
@@ -0,0 +1,18 @@
+
+
+const name: string = "Nodier";
+let hpPoints: number | string = 95;
+let hpPoint2: number | "FULL" = 95;
+
+hpPoints = "FUULL";
+hpPoint2 = "FULL";
+
+
+const isAlive: boolean = true;
+
+console.log({name, hpPoints, hpPoint2, isAlive});
+
+
+
+
+export {}
