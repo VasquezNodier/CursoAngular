fde87bcd16a3ebca79c7b0418d77fc91757d4dac
Nodier Vasquez <vasqueznap@gmail.com>
2024-09-04 22:03:02 -0500

    10 decorators

---
+++ TypeScript-Intro/src/topics/10-decorators.ts
@@ -0,0 +1,25 @@
+
+// (1)
+function classDecorator(constructor: any) {
+     return class extends constructor {
+        newProperty = 'New Property';
+        hello = 'override';
+     }
+}
+
+@classDecorator
+export class SuperClass {
+    public myProperty: string = "Abc123"
+
+    print() {
+        console.log('Hola Mundo')
+    }
+
+}
+
+
+console.log(SuperClass)
+
+const myClass = new SuperClass();
+
+console.log(myClass);
