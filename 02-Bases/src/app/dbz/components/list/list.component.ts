e8c455c9829d55f05dfdc7d13b11f5212d8c155e
Nodier Vasquez <vasqueznap@gmail.com>
2024-11-14 12:39:17 -0500

    60. 61. Dividir app en componentes

---
+++ 02-Bases/src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-dbz-list',
+    templateUrl: './list.component.html',
+    styleUrl: './list.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ListComponent { }
