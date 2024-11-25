6c5ef43935d63a0439cbf041cbef778572926481
Nodier Vasquez <vasqueznap@gmail.com>
2024-11-24 21:26:30 -0500

    91. @ViewChild - Referencia al HTML

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html'
 })
 export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor() { }
+
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
 
 }
