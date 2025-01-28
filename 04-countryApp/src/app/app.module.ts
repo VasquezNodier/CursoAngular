ed698f386dd1bb68b1ccb65a72ab0564fb834397
Nodier Vasquez <vasqueznap@gmail.com>
2025-01-28 14:40:51 -0500

    123 - 124. Country Service y funcionamiento

--- 04-countryApp/src/app/app.module.ts
+++ 04-countryApp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CountriesModule } from './countries/countries.module';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { CountriesModule } from './countries/countries.module';
     SharedModule,
     CountriesModule
   ],
-  providers: [],
+  providers: [ provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/countries';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private url : string = 'https://restcountries.com/v3.1';
+
+  constructor(  private httpClient : HttpClient   ) { }
+
+  searchCapital( term : string): Observable<Country[]>{
+    const url = `${this.url}/capital/${term}`;
+
+    return this.httpClient.get<Country[]>( url );
+  }
+
+}
